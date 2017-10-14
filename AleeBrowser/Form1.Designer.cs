65edcb5131a5aff46373f7a085bc920bfdfdd64a
Alee14 <Alee14498@gmail.com>
2017-10-13 21:00:56 -0400

    Project revived!

--- AleeBrowser/Form1.Designer.cs
+++ AleeBrowser/Form1.Designer.cs
@@ -28,30 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
-            this.statusStrip1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(766, 530);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Please note that AleeBrowser is in alpha";
             // 
             // button3
             // 
@@ -68,18 +53,19 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(819, 512);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(382, 534);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.Size = new System.Drawing.Size(213, 31);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Alpha 0.2 Build 15";
+            this.label2.Text = "AleeBrowser 1.0";
             // 
             // button4
             // 
             this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button4.Location = new System.Drawing.Point(26, 512);
+            this.button4.Location = new System.Drawing.Point(17, 537);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(71, 29);
+            this.button4.Size = new System.Drawing.Size(71, 30);
             this.button4.TabIndex = 6;
             this.button4.Text = "About";
             this.button4.UseVisualStyleBackColor = true;
@@ -90,107 +76,76 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(17, 15);
+            this.textBox1.Location = new System.Drawing.Point(17, 17);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(889, 20);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "http://";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(194, 512);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button1.Location = new System.Drawing.Point(106, 537);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 31);
+            this.button1.Size = new System.Drawing.Size(71, 30);
             this.button1.TabIndex = 10;
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripProgressBar1,
-            this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 552);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(974, 22);
-            this.statusStrip1.TabIndex = 11;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripProgressBar1
-            // 
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
-            this.toolStripProgressBar1.Click += new System.EventHandler(this.toolStripProgressBar1_Click);
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(82, 17);
-            this.toolStripStatusLabel1.Text = "Current Status";
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(103, 510);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 31);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Victorify";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Location = new System.Drawing.Point(18, 47);
+            this.panel1.Location = new System.Drawing.Point(18, 54);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(934, 459);
+            this.panel1.Size = new System.Drawing.Size(934, 469);
             this.panel1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(707, 534);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Made by: AleeCorp";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 574);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "AleeBrowser";
+            this.Text = "AleeBrowser 1.0";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel1;
         private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- AleeBrowser/Form1.cs
+++ AleeBrowser/Form1.cs
@@ -23,6 +23,7 @@ namespace AleeBrowser
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MessageBox.Show("Please note that this software is still working progress so don't expect things to work.");
 
             browser = new CefSharp.WinForms.ChromiumWebBrowser("http://alee14.ga");
             {
@@ -38,7 +39,8 @@ namespace AleeBrowser
 
         private void button4_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("This browser is created by: AleeCorp");
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -52,9 +54,6 @@ namespace AleeBrowser
         }
         private void NavigateToPage()
         {
-            button3.Enabled = false;
-            textBox1.Enabled = false;
-            toolStripProgressBar1.Text = "Navigation has started";
             browser.Load(textBox1.Text);
 
         }
@@ -66,17 +65,9 @@ namespace AleeBrowser
             }
         }
 
-        private void browser_DocumentCompleted(object sender, ChromiumWebBrowser.DocumentCompletedEventArgs e)
-        {
-            button3.Enabled = true;
-            textBox1.Enabled = true;
-            toolStripProgressBar1.Text = "Navigation Complete";
-        }
-
         private void browser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
         {
-            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
-            toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+           
             
             }
 
@@ -85,14 +76,9 @@ namespace AleeBrowser
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-          //  foreach( HtmlElement image in browser.Document.Images)
-          //  {
-          //      image.SetAttribute("src", "https://yt3.ggpht.com/-Iuf1v4-SSSM/AAAAAAAAAAI/AAAAAAAAAAA/89IYeQw--wU/s900-c-k-no-mo-rj-c0xffffff/photo.jpg");
-         //   }
+
         }
     }
         }
-// This project was made in May 8th 2017
-// And here's the "before convert" file of AleeBrowser https://www.dropbox.com/s/16q7ay6otd4njw4/AleeBrowser.exe?dl=0
